0789f9de43c1c92b361ae6a0320000428585beb9
roykirk74 <roykirk74@gmail.com>
2022-12-29 11:04:42 -0700

    initial commit

---
+++ src/app/user/auth-modal/auth-modal.component.ts
@@ -0,0 +1,18 @@
+import { ModalService } from 'src/app/services/modal.service';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-modal',
+  templateUrl: './auth-modal.component.html',
+  styleUrls: ['./auth-modal.component.css']
+})
+export class AuthModalComponent implements OnInit, OnDestroy {
+  constructor(public modal: ModalService) {}
+  ngOnInit(): void {
+    this.modal.register('auth')
+  }
+
+  ngOnDestroy() {
+    this.modal.unregister('auth')
+  }
+}
---
+++ src/app/user/register/register.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent {
+  name = new FormControl('', [
+    Validators.required,
+    Validators.minLength(3)
+  ])
+  email = new FormControl('', [
+    Validators.required,
+    Validators.email
+  ])
+  age = new FormControl('', [
+    Validators.required,
+    Validators.min(18),
+    Validators.max(140)
+  ])
+  password = new FormControl('', [
+    Validators.required,
+    Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm)
+  ])
+  confirmPassword = new FormControl('', [
+    Validators.required
+  ])
+  phoneNumber = new FormControl('', [
+    Validators.required,
+    Validators.min(13),
+    Validators.max(13)
+  ])
+
+  registerForm = new  FormGroup({
+    name: this.name,
+    email: this.email,
+    age: this.age,
+    password: this.password,
+    confirmPassword: this.confirmPassword,
+    phoneNumber: this.phoneNumber
+  })
+}
